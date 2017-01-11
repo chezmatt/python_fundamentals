aa8ece874de413d185742f6a951f2ea3ac0c1232
Matt W <wilkinson.matthew@gmail.com>
2017-01-11 13:17:15 -0800

    final val for form

--- flaskproj/regformval/server.py
+++ flaskproj/regformval/server.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 
-@app.route('/process', methods=['POST'])
-def process():
-  if len(request.form['name']) < 1:
-    flash("Name cannot be empty!") # just pass a string to the flash function
-  else:
-    flash("Success! Your name is {}".format(request.form['name'])) # just pass a string to the flash function
-  return redirect('/') # either way the application should return to the index and display the message
+# @app.route('/')
+# def default():
+#     return render_template('index.html')
+
+@app.route('/')
+def shoeSurvey():
+    return render_template('dojos.html')
+
+@app.route('/survey', methods=['POST'])
+def assembleSurvey():
+    message = ""
+    fname = request.form['fname']
+    loc = request.form['loc']
+    lang = request.form['lang']
+    comment = request.form['comment']
+    print "Post info recvieved !!!!!!!!!!!!"
+
+    if (fname or comment) =="":
+      flash("Cannot be empty!") # just pass a string to the flash
+    else:
+      flash("Success!")
+    #
+    return render_template('results.html', fname =fname,loc=loc,lang=lang,comment=comment,message=message)
+
+
+app.run(debug=True)      # Run the app in debug mode.
